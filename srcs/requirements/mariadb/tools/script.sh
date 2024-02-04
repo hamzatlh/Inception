f6aeabe30fbd13d252f30b92e471e2c1fe9a64b2
hamzatlh <htlhaouia@gmail.com>
2024-02-04 11:15:03 -0500

    upadtes DB

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    chown -R mysql:mysql /var/lib/mysql
-
-    # init database
-    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-    tfile=$(mktemp)
-    if [ ! -f "$tfile" ]; then
-        return 1
-    fi
-fi
-
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+# Check if MariaDB data directory exists
+if [ ! -d "/var/lib/mysql/test" ]; then
+    # Create a temporary SQL file
     cat << EOF > /tmp/create_db.sql
-USE mysql;
 FLUSH PRIVILEGES;
-CREATE DATABASE ${DATABASE_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DATABASE_USER}'@'%' IDENTIFIED by '${DATABASE_USER_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
-ALTER USER '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_USER_PASSWORD}';
+USE mysql;
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+CREATE DATABASE test;
+CREATE USER 'test'@'%' IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON test.* TO 'test'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-    # run init.sql
+    # Stop MariaDB server
+    service mariadb stop
+
+    # Bootstrap MariaDB with the SQL script
     mariadbd --user=mysql --bootstrap < /tmp/create_db.sql
+
+    # Remove temporary SQL file
     rm -f /tmp/create_db.sql
+
+    # Start MariaDB server
+    service mariadb start
 fi
+
+# Execute the provided command (e.g., starting a shell)
+exec "$@"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,24 +5,24 @@ echo "Checking ..."
 #     mkdir -p /var/www/html/wordpress
 # fi
 
-echo "Creating wordpress ..."
+echo "<<< <<< Creating wordpress ..."
 cd /var/www/html/wordpress
 rm -rf /var/www/html/wordpress/*
 
-echo "Installing wordpress ..."
+echo "<<< <<< Installing wordpress ..."
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-echo "Downloading wordpress ..."
+echo "<<< <<< Downloading wordpress ..."
 wp core download --allow-root
 
-echo "Creating wordpress config ..."
-wp config create --allow-root --dbname=${DATABASE_NAME} --dbuser=${DATABASE_USER} --dbpass=${DATABASE_ROOT_PASSWORD} --dbhost=mariadb
+echo "<<< <<< Creating wordpress config ..."
+wp config create --allow-root --dbname=${DATABASE_NAME} --dbuser=${DATABASE_USER} --dbpass=${DATABASE_ROOT_PASSWORD} --dbhost=mariadb:3306
 
-echo "Installing wordpress ..."
+echo "<<< <<< Installing wordpress ..."
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWD} --admin_email=${WP_ADMIN_EMAIL}
 
-echo "Creating user ..."
+echo "<<< <<< Creating user ..."
 wp user create "${WP_USER}" "${WP_EMAIL}" --user_pass="${WP_PASSWORD}" --role=author
 exec "$@"
